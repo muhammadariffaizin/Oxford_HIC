05cd9c0c291269422db69d7ea75271e57377fe61
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2024-06-07 16:53:52 +0700

    feat: add num workers support

--- data/download_img.py
+++ data/download_img.py
@@ -4,7 +4,7 @@ import os
 import grequests
 from tqdm import tqdm
 
-def download_images(batch, save_path):
+def download_images(batch, save_path, num_workers=8):
     urls = []
     image_ids = []
 
@@ -17,7 +17,7 @@ def download_images(batch, save_path):
     # using map
     rs = (grequests.get(url) for url in urls)
     
-    for i, response in tqdm(grequests.imap_enumerated(rs, size=20)):
+    for i, response in tqdm(grequests.imap_enumerated(rs, size=num_workers)):
         if response is None:
             print(f"Failed to download image {image_ids[i]}")
             continue
@@ -30,7 +30,7 @@ def download_images(batch, save_path):
         with open(image_path, 'wb') as image_file:
             image_file.write(response.content)
 
-def download_images_from_csv(file_path, save_path, batch_size):
+def download_images_from_csv(file_path, save_path, batch_size=1000, num_workers=8):
     # Open the CSV file
     with open(file_path, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
@@ -49,13 +49,14 @@ def download_images_from_csv(file_path, save_path, batch_size):
             except StopIteration:
                 break
 
-            download_images(batch, save_path)
+            download_images(batch, save_path, num_workers)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download images from CSV file')
     parser.add_argument('--file_path', type=str, default='./hic_data/oxford_hic_image_info.csv')
     parser.add_argument('--save_path', type=str, default='./hic_data/images')
     parser.add_argument('--batch_size', type=int, default=10000)
+    parser.add_argument('--num_workers', type=int, default=8)
     args = parser.parse_args()
     return args
 
@@ -66,7 +67,7 @@ def main():
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
         
-    download_images_from_csv(args.file_path, args.save_path, args.batch_size)
+    download_images_from_csv(args.file_path, args.save_path, args.batch_size, args.num_workers)
 
 
 if __name__ == '__main__':
